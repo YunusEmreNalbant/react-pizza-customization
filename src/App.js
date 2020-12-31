11948a4eac3d92d2ce158b06d73392fc91a83145
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2020-12-31 15:14:54 +0300

    componentler olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Header from "./components/Header";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Checkout from "./components/Checkout";
+import Customize from "./components/Customize";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/">
+                        <Customize/>
+                    </Route>
+                    <Route path="/checkout">
+                        <Checkout/>
+                    </Route>
+
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
---
+++ src/components/Customize.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Cheese from "../assets/BaseCheese.png";
+import Base from "../assets/PizzaBase.png";
+import Olive from "../assets/Olive.png";
+import Pineapple from "../assets/Pineapple.png";
+import Mushroom from "../assets/Mushroom.png";
+import Basil from "../assets/Basil.png";
+import Tomato from "../assets/Tomato.png";
+
+const Customize = () => {
+    return (
+        <div style={{display: 'flex'}}>
+            <div style={{border: '2px solid black', flex: 1}}>
+                <div style={{maxHeight: 500, maxWidth: 500, position: 'relative'}}>
+                    <img src={Cheese} alt={"Pizza Base"} height={'100%'} width={'100%'} className={'ingredients'}/>
+                    <img src={Olive} alt={"Pizza Base"} height={'100%'} width={'100%'} className={'ingredients'}/>
+                    <img src={Pineapple} alt={"Pizza Base"} height={'100%'} width={'100%'} className={'ingredients'}/>
+                    <img src={Mushroom} alt={"Pizza Base"} height={'100%'} width={'100%'} className={'ingredients'}/>
+                    <img src={Basil} alt={"Pizza Base"} height={'100%'} width={'100%'} className={'ingredients'}/>
+                    <img src={Tomato} alt={"Pizza Base"} height={'100%'} width={'100%'} className={'ingredients'}/>
+                    <img src={Base} alt={"Pizza Base"} height={'100%'} width={'100%'} />
+
+                </div>
+            </div>
+            <div style={{border: '2px solid black', flex: 1}}>
+                Checkbox
+            </div>
+        </div>
+    );
+};
+
+export default Customize;
