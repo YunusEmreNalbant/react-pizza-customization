f48052d8cb55238532a0d356b4cf73965a7589d9
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-01-01 19:21:37 +0300

    animasyonlar eklendi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./components/Header";
 import {
     BrowserRouter as Router,
@@ -8,25 +8,35 @@ import {
 } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Customize from "./components/Customize";
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/">
-                        <Customize/>
-                    </Route>
-                    <Route path="/checkout">
-                        <Checkout/>
-                    </Route>
 
-                </Switch>
-            </div>
-        </Router>
-    </div>
-  );
+function App() {
+    const [ingredients, setIngredients] = useState({
+        //  onions: false,
+        basil: false,
+        cheese: false,
+        mushroom: false,
+        olive: false,
+        pineapple: false,
+        tomato: false,
+    });
+    return (
+        <div className="App">
+            <Header/>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/">
+                            <Customize ingredients={ingredients} setIngredients={setIngredients}/>
+                        </Route>
+                        <Route path="/checkout">
+                            <Checkout/>
+                        </Route>
+
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
